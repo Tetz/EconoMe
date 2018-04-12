378b44ad4fa70d88053e3a0b3deba784dba374f9
Tetsuro Takemoto <t@ineeza.com>
2018-04-12 21:42:02 +0900

    import SnapKit

---
+++ otaku-wallet-ios/otaku-wallet-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  otaku-wallet-ios
+//
+//  Created by Tetsuro Takemoto on 2018/04/12.
+//  Copyright © 2018 Tetsuro Takemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
