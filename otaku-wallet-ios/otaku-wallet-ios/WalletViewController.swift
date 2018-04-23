8306926ebded69d521e0695e1b9ec83c64c5cbb9
Tetsuro Takemoto <t@ineeza.com>
2018-04-23 23:29:12 +0900

    add text labels

--- otaku-wallet-ios/otaku-wallet-ios/WalletViewController.swift
+++ otaku-wallet-ios/otaku-wallet-ios/WalletViewController.swift
@@ -28,14 +28,59 @@ final class WalletViewController: UIViewController {
             make.edges.equalToSuperview()
         }
 
-        let walletcard = UIView()
-        walletcard.backgroundColor = UIColor.white
-        container.addSubview(walletcard)
-        walletcard.snp.makeConstraints { make in
+        let walletcontent = UIView()
+        walletcontent.backgroundColor = UIColor.yellow
+        container.addSubview(walletcontent)
+        walletcontent.snp.makeConstraints { make in
             make.width.height.equalTo(self.view.frame.size.height - 98)
             make.width.width.equalTo(container)
             make.centerX.equalTo(container)
-            make.top.equalTo(22)
+            make.centerY.equalTo(container)
+        }
+        
+        let otakucoinAmount = UILabel()
+        walletcontent.addSubview(otakucoinAmount)
+        otakucoinAmount.text = "5000 OTC"
+        otakucoinAmount.font =  UIFont.systemFont(ofSize: 40.0)
+        otakucoinAmount.snp.makeConstraints { make in
+            make.centerX.equalTo(container)
+            make.top.equalTo(walletcontent).offset(40)
+        }
+        
+        let myEthAddressLabel = UILabel()
+        walletcontent.addSubview(myEthAddressLabel)
+        myEthAddressLabel.text = "My Ether Wallet Address:"
+        myEthAddressLabel.font =  UIFont.systemFont(ofSize: 25.0)
+        myEthAddressLabel.snp.makeConstraints { make in
+            make.left.equalTo(walletcontent).offset(10)
+            make.top.equalTo(otakucoinAmount.snp.bottom).offset(40)
+        }
+        
+        let myEthAddress = UILabel()
+        walletcontent.addSubview(myEthAddress)
+        myEthAddress.text = "0x9aE1f5ADFcc383B1C5a85e7f0BBaD4b768e7D661"
+        myEthAddress.font =  UIFont.systemFont(ofSize: 14.0)
+        myEthAddress.snp.makeConstraints { make in
+            make.left.equalTo(walletcontent).offset(10)
+            make.top.equalTo(myEthAddressLabel.snp.bottom).offset(0)
+        }
+        
+        let ethAmountLabel = UILabel()
+        walletcontent.addSubview(ethAmountLabel)
+        ethAmountLabel.text = "ETH Amount:"
+        ethAmountLabel.font =  UIFont.systemFont(ofSize: 25.0)
+        ethAmountLabel.snp.makeConstraints { make in
+            make.left.equalTo(walletcontent).offset(10)
+            make.top.equalTo(myEthAddress.snp.bottom).offset(20)
+        }
+        
+        let ethAmount = UILabel()
+        walletcontent.addSubview(ethAmount)
+        ethAmount.text = "2.3 ETH"
+        ethAmount.font =  UIFont.systemFont(ofSize: 20.0)
+        ethAmount.snp.makeConstraints { make in
+            make.left.equalTo(walletcontent).offset(10)
+            make.top.equalTo(ethAmountLabel.snp.bottom).offset(0)
         }
         
         let sendButton = UIButton(type: .system)
@@ -58,15 +103,15 @@ final class WalletViewController: UIViewController {
         sendButton.backgroundColor = UIColor.red
         sendButton.snp.makeConstraints { make in
             make.width.width.equalTo(self.view.frame.size.width/2 - 15)
-            make.left.equalTo(walletcard).offset(10)
-            make.bottom.equalTo(walletcard)
+            make.left.equalTo(walletcontent).offset(10)
+            make.bottom.equalTo(walletcontent).offset(-10)
         }
 
         receiveButton.backgroundColor = UIColor.blue
         receiveButton.snp.makeConstraints { make in
             make.width.width.equalTo(self.view.frame.size.width/2 - 15)
             make.left.equalTo(sendButton.snp.right).offset(10)
-            make.bottom.equalTo(walletcard)
+            make.bottom.equalTo(walletcontent).offset(-10)
         }
 
     }
