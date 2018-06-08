f9617e4a61096fd151f8ef283912c906abeaf357
Tetsuro Takemoto <t@ineeza.com>
2018-06-08 17:47:18 +0900

    Remove empty file

--- econome-wallet-ios/econome-wallet-ios/API/Http/SearchTokensResponse.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SearchTokensResponse.swift
-//  econome-wallet-ios
-//
-//  Created by Tetsuro Takemoto on 2018/06/08.
-//  Copyright © 2018 Tetsuro Takemoto. All rights reserved.
-//
-
-import Foundation
