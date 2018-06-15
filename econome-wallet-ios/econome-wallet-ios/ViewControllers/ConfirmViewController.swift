6562869e3d381f147852804203a41d8fc913629e
Tetsuro Takemoto <t@ineeza.com>
2018-06-15 16:22:27 +0900

    Add Labels for ConfirmView

--- econome-wallet-ios/econome-wallet-ios/ViewControllers/ConfirmViewController.swift
+++ econome-wallet-ios/econome-wallet-ios/ViewControllers/ConfirmViewController.swift
@@ -31,11 +31,72 @@ final class ConfirmViewController: UIViewController {
 
         let button = UIButton()
         container.addSubview(button)
+
+        let addressTitleLabel = UILabel()
+        container.addSubview(addressTitleLabel)
+        addressTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        addressTitleLabel.textColor = UIColor.darkGray
+        addressTitleLabel.text = "Recipient"
+        addressTitleLabel.snp.makeConstraints { make in
+            make.left.equalTo(container).offset(20)
+            make.top.equalTo(container).offset(100)
+        }
+
+        let addressLabel = UILabel()
+        container.addSubview(addressLabel)
+        addressLabel.font = UIFont.systemFont(ofSize: 20)
+        addressLabel.textColor = UIColor.black
+        addressLabel.text = "0x..."
+        addressLabel.snp.makeConstraints { make in
+            make.left.equalTo(container).offset(20)
+            make.top.equalTo(addressTitleLabel.snp.bottom).offset(10)
+        }
+
+        let amountTitleLabel = UILabel()
+        container.addSubview(amountTitleLabel)
+        amountTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        amountTitleLabel.textColor = UIColor.darkGray
+        amountTitleLabel.text = "Amount"
+        amountTitleLabel.snp.makeConstraints { make in
+            make.left.equalTo(container).offset(20)
+            make.top.equalTo(addressLabel.snp.bottom).offset(20)
+        }
+
+        let amountLabel = UILabel()
+        container.addSubview(amountLabel)
+        amountLabel.font = UIFont.systemFont(ofSize: 20)
+        amountLabel.textColor = UIColor.black
+        amountLabel.text = "0.0 ETH"
+        amountLabel.snp.makeConstraints { make in
+            make.left.equalTo(container).offset(20)
+            make.top.equalTo(amountTitleLabel.snp.bottom).offset(10)
+        }
+
+        let gasTitleLabel = UILabel()
+        container.addSubview(gasTitleLabel)
+        gasTitleLabel.font = UIFont.systemFont(ofSize: 20)
+        gasTitleLabel.textColor = UIColor.darkGray
+        gasTitleLabel.text = "Est. Network Fees"
+        gasTitleLabel.snp.makeConstraints { make in
+            make.left.equalTo(container).offset(20)
+            make.top.equalTo(amountLabel.snp.bottom).offset(20)
+        }
+
+        let gasLabel = UILabel()
+        container.addSubview(gasLabel)
+        gasLabel.font = UIFont.systemFont(ofSize: 20)
+        gasLabel.textColor = UIColor.black
+        gasLabel.text = "0.0 ETH"
+        gasLabel.snp.makeConstraints { make in
+            make.left.equalTo(container).offset(20)
+            make.top.equalTo(gasTitleLabel.snp.bottom).offset(10)
+        }
+
         // rgb(52, 152, 219)
         let buttonColor = UIColor(red: 52/255.0, green: 152/255.0, blue: 219/255.0, alpha: 1.0)
         button.backgroundColor = buttonColor
         button.layer.cornerRadius = 5
-        button.setTitle("Send", for: .normal)
+        button.setTitle("Confirm", for: .normal)
         button.addTarget(self, action: #selector(self.buttonTapped(_:)), for: .touchUpInside)
         // SnapKit
         button.snp.makeConstraints { make in
